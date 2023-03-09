2835af8b8bd117714b90b8d368387d172a9228fc
Lai In Chang <lavender880331@gmail.com>
2023-03-09 13:21:02 +0100

    first step - render the form

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+class ProjectInput {
+  //get access to all the element we need
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedHtmlContent = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedHtmlContent.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const projectInput = new ProjectInput();
