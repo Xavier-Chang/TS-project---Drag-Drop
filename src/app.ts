6fd743cbf52019aaa063ffc3b2dd5bb7698227fd
Lai In Chang <lavender880331@gmail.com>
2023-03-10 13:21:13 +0100

    add autobind decorator and interact with DOM elements

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,30 @@
+// autobind decorator
+function autobind(
+  _: any,
+  _2: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+// ProjectInpit Class
 class ProjectInput {
   //get access to all the element we need
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -15,7 +37,30 @@ class ProjectInput {
       true
     );
     this.element = importedHtmlContent.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    )! as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    )! as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    )! as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value); //this method will not point to the class without bind
+    console.log(this.descriptionInputElement.value);
+    console.log(this.peopleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler); //bind (this) means that inside of the submitHandler will refer to the same thing as this refers to in this context
   }
 
   private attach() {
